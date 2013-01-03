e77dc22492ad3d70e0fbb2fae0251b6c3e712f55
Fredrik Larsson <fredrik@sharpless.se>
2013-01-03 20:47:05 +0100

    h1h6

--- src/CCTheme/CCTheme.php
+++ src/CCTheme/CCTheme.php
@@ -44,4 +44,11 @@
         }
 
       }
-    }
+     /**
+      * Display text as h1h6 and paragraphs with some inline formatting.
+      */
+        public function H1H6() {
+          $this->views->SetTitle('Theme testing headers and paragraphs')
+                      ->AddInclude(__DIR__ . '/h1h6.tpl.php', array(), 'primary');
+        }
+}
